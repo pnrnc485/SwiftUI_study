18fdf5849843a716f4f262348c60546f02401b6e
Kenta Suzuki <kenta.suzuki1014@gmail.com>
2023-03-16 14:53:24 +0900

    Add a Category View

---
+++ SwiftUICheck/ComposingComplexInterfaces/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  SwiftUICheck
+//
+//  Created by 鈴木健太 on 2023/03/16.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
