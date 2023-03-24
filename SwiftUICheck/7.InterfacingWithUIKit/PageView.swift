1c8dc932b979a92332cb54803f498fdf3d6fffa2
Kenta Suzuki <kenta.suzuki1014@gmail.com>
2023-03-24 12:08:48 +0900

    Create a View to Represent a UIPageViewController

---
+++ SwiftUICheck/7.InterfacingWithUIKit/PageView.swift
@@ -0,0 +1,25 @@
+//
+//  PageView.swift
+//  SwiftUICheck
+//
+//  Created by 鈴木健太 on 2023/03/24.
+//
+
+import SwiftUI
+
+struct PageView<Page: View>: View {
+    var pages: [Page]
+    
+    var body: some View {
+        PageViewController(pages: pages)
+    }
+}
+
+struct PageView_Previews: PreviewProvider {
+    static var previews: some View {
+        PageView(pages: ModelData().features.map {
+            FeatureCard(landmark: $0)
+        })
+        .aspectRatio(3 / 2, contentMode: .fit)
+    }
+}
